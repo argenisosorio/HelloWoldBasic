47a5c4f60402dbe949564b8a48ca565a5cf6d328
Argenis Osorio <argenisosorio580@hotmail.com>
2023-05-13 17:17:30 -0400

    Completado el componente WebView básico, ahora muestra google.com al abrirse

--- app/src/main/java/com/example/hellowoldbasic/MainActivity.java
+++ app/src/main/java/com/example/hellowoldbasic/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.hellowoldbasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    private WebSettings myWebSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.web);
+        myWebSettings =  myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+        myWebSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://www.google.com/");
+        myWebView.setWebViewClient(new WebViewClient());
     }
 }
