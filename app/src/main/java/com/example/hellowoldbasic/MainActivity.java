de9fe1b898d107c7db421957d19e3a2ec9b3d456
Argenis Osorio <argenisosorio580@hotmail.com>
2023-06-05 15:56:33 -0400

    Agregadas modificaciones para realizar la consulta a un endpoint mostrar la información en un TextView

--- app/src/main/java/com/example/hellowoldbasic/MainActivity.java
+++ app/src/main/java/com/example/hellowoldbasic/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.hellowoldbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.resultTextView);
+
+        // Realizar la solicitud a la API en segundo plano
+        new FetchDataAsyncTask().execute();
+    }
+
+    private class FetchDataAsyncTask extends AsyncTask<Void, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Void... params) {
+            int result = 0;
+            try {
+                URL url = new URL("https://sinidot.fundavene.gob.ve/api/countdonor");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                connection.connect();
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    JSONObject jsonResponse = new JSONObject(response.toString());
+                    result = jsonResponse.getInt("result");
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            // Actualizar el contenido del TextView con el número obtenido de la API
+            textView.setText("Hello World " + result);
+        }
     }
 }
